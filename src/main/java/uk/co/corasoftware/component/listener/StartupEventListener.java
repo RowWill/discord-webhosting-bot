88c89ba2f51c82eea094b10d5739e3ddc45f7b30
RowWill <rowan.williams1989@gmail.com>
2020-09-27 00:12:47 +0100

    Test data now generated on startup.
    - Test data is created on server startup if there is no root token
    present.
    - Data no longer created on per GET request

--- src/main/java/uk/co/corasoftware/component/listener/StartupEventListener.java
+++ src/main/java/uk/co/corasoftware/component/listener/StartupEventListener.java
@@ -1,6 +1,8 @@
 package uk.co.corasoftware.component.listener;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,8 +12,18 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 import uk.co.corasoftware.controller.security.SecurityTokenController;
+import uk.co.corasoftware.enums.ApiType;
+import uk.co.corasoftware.enums.InstanceType;
+import uk.co.corasoftware.enums.NodeType;
+import uk.co.corasoftware.enums.ScalingMode;
 import uk.co.corasoftware.enums.TokenType;
+import uk.co.corasoftware.model.ServiceProduct;
 import uk.co.corasoftware.model.security.ApiToken;
+import uk.co.corasoftware.model.server.Environment;
+import uk.co.corasoftware.model.server.Node;
+import uk.co.corasoftware.repo.ServiceProductRepo;
+import uk.co.corasoftware.repo.server.EnvironmentRepo;
+import uk.co.corasoftware.repo.server.NodeRepo;
 import uk.co.corasoftware.util.security.jwt.JwtTokenEncoder;
 
 @Component
@@ -21,6 +33,15 @@ public class StartupEventListener implements ApplicationListener<ContextRefreshe
 
 	@Autowired
 	private SecurityTokenController securityTokenController;
+
+	@Autowired
+	private ServiceProductRepo serviceProductRepo;
+
+	@Autowired
+	private EnvironmentRepo environmentRepo;
+
+	@Autowired
+	private NodeRepo nodeRepo;
 
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
@@ -40,6 +61,33 @@ public class StartupEventListener implements ApplicationListener<ContextRefreshe
 			// @formatter:on
 			securityTokenController.save(token);
 			LOG.info("Root token generated");
+
+			/*
+			 * DEBUG
+			 */
+			generateTestData();
 		}
+	}
+
+	private void generateTestData() {
+		Environment environment = Environment.builder().shortDomain("test-domain").region("LON-1").isHaEnabled(false)
+				.sslState(false).build();
+
+		List<Node> nodes = new ArrayList();
+		for (int i = 1; i <= 5; i++) {
+			Node node = Node.builder().displayName("node-" + UUID.randomUUID().toString()).nodeType(NodeType.NODEJS)
+					.nodeGroup("cp" + i).diskLimit(10).extip(0).extipv6(0).fixedCloudlets(1).flexibleCloudlets(4)
+					.restartDelay(30).tag("tag-" + i).scalingMode(ScalingMode.STATEFUL).build();
+			nodes.add(node);
+		}
+
+		nodeRepo.saveAll(nodes);
+		environment = environmentRepo.save(environment);
+
+		ServiceProduct service = ServiceProduct.builder().name("Test Product").description("Test Product Description")
+				.environment(environment).instanceType(InstanceType.SINGLE_APPLICATION_SERVER).apiType(ApiType.JELASTIC)
+				.nodes(nodes).build();
+
+		service = serviceProductRepo.save(service);
 	}
 }
--- src/main/java/uk/co/corasoftware/controller/TestApi.java
+++ src/main/java/uk/co/corasoftware/controller/TestApi.java
@@ -1,9 +1,5 @@
 package uk.co.corasoftware.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -13,16 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import uk.co.corasoftware.controller.security.SecurityTokenController;
-import uk.co.corasoftware.enums.ApiType;
-import uk.co.corasoftware.enums.InstanceType;
-import uk.co.corasoftware.enums.NodeType;
-import uk.co.corasoftware.enums.ScalingMode;
 import uk.co.corasoftware.exception.InvalidSecurityTokenException;
 import uk.co.corasoftware.model.ServiceProduct;
-import uk.co.corasoftware.model.server.Environment;
-import uk.co.corasoftware.model.server.Node;
-import uk.co.corasoftware.repo.IssuedServiceRepo;
 import uk.co.corasoftware.repo.ServiceProductRepo;
 import uk.co.corasoftware.repo.server.EnvironmentRepo;
 import uk.co.corasoftware.repo.server.NodeRepo;
@@ -32,13 +20,7 @@ import uk.co.corasoftware.repo.server.NodeRepo;
 public class TestApi {
 
 	@Autowired
-	private SecurityTokenController securityTokenController;
-
-	@Autowired
 	private ServiceProductRepo serviceProductRepo;
-
-	@Autowired
-	private IssuedServiceRepo issuedServiceRepo;
 
 	@Autowired
 	private EnvironmentRepo environmentRepo;
@@ -61,46 +43,9 @@ public class TestApi {
 	public ResponseEntity<ServiceProduct> testApi(@RequestParam String token) throws InvalidSecurityTokenException {
 		// @formatter:off
 		
-		Environment environment = Environment.builder()
-				.shortDomain("test-domain")
-				.region("LON-1")
-				.isHaEnabled(false)
-				.sslState(false)
-				.build();
+		ServiceProduct product = serviceProductRepo.findAll().get(0);
 		
-		List<Node> nodes = new ArrayList();
-		for(int i = 1; i <= 5; i++) {
-			Node node = Node.builder()
-					.displayName("node-" + UUID.randomUUID().toString())
-					.nodeType(NodeType.NODEJS)
-					.nodeGroup("cp" + i)
-					.diskLimit(10)
-					.extip(0)
-					.extipv6(0)
-					.fixedCloudlets(1)
-					.flexibleCloudlets(4)
-					.restartDelay(30)
-					.tag("tag-" + i)
-					.scalingMode(ScalingMode.STATEFUL)
-					.build();
-			nodes.add(node);
-		}
-		
-		nodeRepo.saveAll(nodes);
-		environment = environmentRepo.save(environment);
-		
-		ServiceProduct service = ServiceProduct.builder()
-				.name("Test Product")
-				.description("Test Product Description")
-				.environment(environment)
-				.instanceType(InstanceType.SINGLE_APPLICATION_SERVER)
-				.apiType(ApiType.JELASTIC)
-				.nodes(nodes)
-				.build();
-		
-		service = serviceProductRepo.save(service);
-		
-		return new ResponseEntity<ServiceProduct>(service, HttpStatus.OK);
+		return new ResponseEntity<ServiceProduct>(product, HttpStatus.OK);
 		// @formatter:on
 	}
 }
